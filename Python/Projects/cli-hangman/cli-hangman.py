31d40296165aaf24c17fa2b5aec1bba59e30aa23
Wellington Campos <wellington.albertoni@gmail.com>
2022-05-11 15:14:55 -0400

    Add games tic-tac-toe and cli-hangman - Reestructure files - Add tic-tac-toe - Add cli-hangman - Remove duplicated Files

--- Python/Projects/cli-hangman/cli-hangman.py
+++ Python/Projects/cli-hangman/cli-hangman.py
@@ -1,7 +1,11 @@
 ''' Jogo da forca simples usando listas '''
-from random import choice
+from os import system
 
-palavras = ['perfume', 'batata', 'alface', 'tomate']
+from random import choice
+from unidecode import unidecode
+
+from palavras import palavras
+
 digitadas = []
 erros = 0
 palavra_secreta = ''
@@ -12,7 +16,8 @@ class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
+    OKGREEN = '\033[90m'
+    OKYELLOW = '\033[93m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
@@ -44,40 +49,54 @@ def novo_jogo():
 print(palavra_secreta)
 def revelar_letra(letra):
     for i, x in enumerate(palavra_secreta):
-        if x == letra:
-            escondida[i] = palavra_secreta[i]
-    return escondida
+        if unidecode(x) == letra:
+            escondida[i] = x
+    
+def formatar(obj):
+    return ' '.join(obj)
+
+def limpar_tela():
+    system('clear')
 
 novo_jogo()
+limpar_tela()
 while True:
     try:
-        letra = input('Informe uma letra: ')
+        print(f'\n\t\t{formatar(escondida)}\n\n')
+        letra = input('Informe uma letra (sem acento): ')
 
         if len(letra) > 1:
+            limpar_tela()
             print("Digite apenas uma letra.\n\n")
             continue
-        
+         
         if letra.lower() not in 'abcdefghijklmnopqrstuvwxyz':
+            limpar_tela()
             print("Isso não é uma letra\n\n")
             continue
         
         if letra.lower() in digitadas:
+            limpar_tela()
             print("Letra repetida. \n\n")
+            print(f'Palavras que já foram:\n {formatar(digitadas)}\n')
             continue
 
         letra = letra.lower()
     
         digitadas.append(letra)
         
-        if letra in palavra_secreta:
-            print(f'{bcolors.OKGREEN} Essa letra tem, hein! {bcolors.ENDC}')
-            print(f'{revelar_letra(letra)}\n\n')
+        if letra in unidecode(palavra_secreta):
+            limpar_tela()
+            print(f'{bcolors.OKYELLOW} Essa letra tem, hein! {bcolors.ENDC}')
+            revelar_letra(letra)
 
         else:
+            limpar_tela()
             print(f"{bcolors.FAIL} Elou! {bcolors.ENDC}\n\n")
             erros += 1
 
             if erros == 6:
+                limpar_tela()
                 print(f''' 
                 Fim de Jogo.
 
@@ -88,14 +107,13 @@ while True:
                 continue
 
         if '_' not in escondida:
-            print("PARABÉNS VOCÊ ACERTOU!!!")
+            limpar_tela()
+            print(f'A palavra era: {palavra_secreta}')
+            print(f'{bcolors.OKCYAN} PARABÉNS VOCÊ ACERTOU!!! {bcolors.ENDC}')
             novo_jogo()
             continue
 
-        print(f'Letras já digitadas: {digitadas}\n\n')
-
-            
-
+        print(f'Letras já digitadas: \n\n {formatar(digitadas)}\n\n')
 
     except EOFError:
         print("Usuário vacilão não digitou nada")
