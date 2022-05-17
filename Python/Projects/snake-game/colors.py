c429dde8e27a59ea9e74477766e77cb457c24ee7
Wellington Campos <wellington.albertoni@gmail.com>
2022-05-17 17:51:36 -0400

    Bug fixes of snake_game   - Add colors file   - Requirements.txt   - Snake_Game.py

---
+++ Python/Projects/snake-game/colors.py
@@ -0,0 +1,5 @@
+class Color():
+    bg_color = (0, 0, 0)
+    body = (0, 255, 0)
+    fruit_melon = (255, 255, 0)
+    head = (255, 0, 0)
---
+++ Python/Projects/snake-game/snake_game.py
@@ -0,0 +1,99 @@
+import sys, pygame
+
+from pygame import K_UP, K_RIGHT, K_LEFT, K_DOWN
+
+from colors import Color
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Snake-game')
+direction = K_RIGHT 
+game_over = False
+
+clock = pygame.time.Clock()
+
+body = list()
+
+def switch(old, new):
+    old[0] = new[0]
+    old[1] = new[1]
+
+def move_snake(key_pressed = None):
+    global snake_head
+    global body
+    
+    steps = 10
+    if body:
+        tam_corpo = len(body)
+        if tam_corpo >= 2:
+            i = 2
+            for j in reversed(body):
+                if (tam_corpo - i) == 0:
+                    switch(j, body[0])
+                    break
+
+                switch(j, body[tam_corpo - i])
+                i += 1
+
+        switch(body[0], snake_head)
+
+    if key_pressed == K_UP:
+        snake_head[1] -= steps
+    elif key_pressed == K_DOWN:
+        snake_head[1] += steps
+    elif key_pressed == K_LEFT:
+        snake_head[0] -= steps
+    elif key_pressed == K_RIGHT:
+        snake_head[0] += steps
+
+allowed_keys = [K_UP, K_RIGHT, K_LEFT, K_DOWN]
+
+fruit = pygame.draw.rect(screen, Color.fruit_melon, (500, 300, 10, 10))
+snake_head = pygame.draw.rect(screen, Color.head, (400, 300, 10, 10))
+
+while not game_over:
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT): 
+            sys.exit()
+        if (event.type == pygame.KEYDOWN):
+            pressed_key = event.key 
+            if (pressed_key in allowed_keys):
+                move_snake(pressed_key)
+                direction = pressed_key
+
+    move_snake(direction)
+
+    # Verifica se não encostou/ultrapassou o limite da tela
+    if snake_head.x < 0 or snake_head.y < 0 or snake_head.x >= 800 or snake_head.y >= 600:
+        game_over = True
+    
+    if body:
+        # Verifica se houve colisão com o próprio corpo
+        if len(snake_head.collidelistall(body)) > 0:
+            game_over = True
+            break
+
+        # desenha todos os componentes do corpo
+        for i in body:
+            pygame.draw.rect(screen, Color.body, i)
+
+    # Verifica se houve colisão da cabeça com a fruta 
+    if (fruit.colliderect(snake_head)):
+        if not body:
+            body.append(snake_head.move(-11, 0))
+        else:
+            body.append(tail.move(-11, 0))
+
+        tail = body[-1]
+        fruit = fruit.move(30, 0)
+
+    snake_head = pygame.draw.rect(screen, Color.head, snake_head)
+    fruit = pygame.draw.rect(screen, Color.fruit_melon, fruit)
+            
+    clock.tick(15)
+    pygame.display.flip()
+    screen.fill(Color.bg_color)
+
+pygame.quit()
+
