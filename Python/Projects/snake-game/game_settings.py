234190e52949c8491aee33ec9c6794698f244fd6
Wellington Campos <wellington.albertoni@gmail.com>
2022-05-18 10:07:10 -0400

    Add game_settings file on snake_game

---
+++ Python/Projects/snake-game/game_settings.py
@@ -0,0 +1,11 @@
+import pygame
+
+# SCREEN SETTINGS
+height = 800
+width = 600
+
+#  
+start_direction = pygame.K_RIGHT
+steps = 10 
+
+allowed_keys = [pygame.K_UP, pygame.K_RIGHT, pygame.K_LEFT, pygame.K_DOWN]
